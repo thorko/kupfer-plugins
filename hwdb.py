012e391e411eb03118ea548fdfd77be853f11dcd
Thorsten Müller <t.mueller@denic.de>
2018-07-12 11:23:23 +0200

    hwdb rewritten

--- hwdb.py
+++ hwdb.py
@@ -1,52 +1,45 @@
-__kupfer_name__ = _("HWDB")
-__kupfer_actions__ = ("Hwdb", )
-__description__ = _("get info from hwdb")
-__version__ = "2018-2"
+__kupfer_name__ = _("HwDB")
+__kupfer_actions__ = ("HwDB", )
+__description__ = _("Search hwdb")
+__version__ = "2018-6"
 __author__ = "thorko"
 
-import gi
-gi.require_version('Notify', '0.7')
+from kupfer.objects import Action, TextLeaf
+from kupfer import utils, plugin_support, config
 
-from kupfer.objects import Action, Leaf, TextLeaf
-from kupfer import icons, uiutils
-from kupfer import utils
-import subprocess
-from gi.repository import Notify
+_ALTERNATIVES = (
+        "google-chrome-stable",
+        "chromium",
+        "firefox",
+        "vivaldi-stable",
+)
 
-class Hwdb (Action):
+__kupfer_settings__ = plugin_support.PluginSettings(
+        {
+            "key" : "browser_type",
+            "label": _("Your browser to use"),
+            "type": str,
+            "value": "google-chrome-stable",
+            "alternatives": _ALTERNATIVES,
+        }
+)
+
+
+class HwDB (Action):
     def __init__(self):
-        Action.__init__(self, _("hwdb"))
+        Action.__init__(self, _("HwDB"))
 
-    def wants_context(self):
-        return True
-
-    def activate(self, leaf, ctx):
-        #cmd = ['ssh x10734 "hwdb ip" | grep "%s"' % leaf.object]
-        cmd = ['ssh', 'x10734', 'hwdb', 'ip', '|', 'grep', '"%s"' % leaf.object]
-        #info = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-        info = ""
-        p = subprocess.Popen(cmd,stdout=subprocess.PIPE)
-        for line in p.stdout.readlines():
-            info += line.decode("utf-8")
-
-        Notify.init("HWDB info")
-        if len(info): 
-            result = info
-        else:
-            result = "No information available"
-
-        # display notification window
-        note = Notify.Notification.new(leaf.object, result)
-        note.set_timeout(6000)
-        note.show()
-
+    def activate(self, leaf):
+        browser_type = __kupfer_settings__["browser_type"]
+        searchhwdb = [browser_type, 'https://nannyfe.prod.denic.de/search?Search=Search&searchpattern=%s' % leaf.object]
+        utils.spawn_async(searchhwdb)
 
     def item_types(self):
         yield TextLeaf
 
     def get_description(self):
-        return _("Get information from hwdb")
+        return _("Search hwdb in browser")
 
     def get_icon_name(self):
-        return "edit-find"
+        return "hwdb"
 
