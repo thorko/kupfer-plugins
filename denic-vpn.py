9f276c499ce17b286e68dbf3ca419c46ef45bde8
Thorsten Müller <t.mueller@denic.de>
2018-09-12 16:38:10 +0200

    updated kupfer plugins

---
+++ denic-vpn.py
@@ -0,0 +1,57 @@
+__kupfer_name__ = _("VPN Denic")
+__kupfer_sources__ = ("VPN",)
+__description__ = _("handle VPN connections")
+__version__ = "2018-9"
+__author__ = "thorko"
+
+from kupfer.objects import Source, RunnableLeaf
+from kupfer import utils, plugin_support, config
+
+class VPN (Source):
+    def __init__(self):
+        super().__init__(_("VPN denic"))
+
+    def initialize(self):
+        pass
+
+    def finalize(self):
+        pass
+
+    def get_items(self):
+        yield Start()
+        yield Stop()
+
+    def provides(self):
+        yield RunnableLeaf
+
+    def description(self):
+        return __description__
+    
+    def get_icon_name(self):
+        return "im-user"
+
+class Start (RunnableLeaf):
+    def __init__(self):
+        super().__init__(name=_("Start"))
+
+    def run(self):
+        vpnstart=["/usr/local/bin/vpnconnect.sh", "start"]
+        utils.spawn_async(vpnstart)
+
+    def item_types(self):
+        yield TextLeaf
+
+    def get_icon_name(self):
+        return "im-user-online"
+
+
+class Stop (RunnableLeaf):
+    def __init__(self):
+        super().__init__(name=_("Stop"))
+
+    def run(self):
+        vpnstop=["/usr/local/bin/vpnconnect.sh", "stop"]
+        utils.spawn_async(vpnstop)
+
+    def get_icon_name(self):
+        return "im-kick-user"
--- hwdb.py
+++ hwdb.py
@@ -4,9 +4,9 @@ __description__ = _("Search hwdb")
 __version__ = "2018-6"
 __author__ = "thorko"
 
-from kupfer.objects import Action, TextLeaf
+from kupfer.objects import Action, TextLeaf, Leaf, RunnableLeaf, Source
 from kupfer import utils, plugin_support, config
-import re
+import pprint
 
 _ALTERNATIVES = (
         "google-chrome-stable",
@@ -30,15 +30,15 @@ class HwDB (Action):
     def __init__(self):
         Action.__init__(self, _("HwDB"))
 
-    def activate(self, leaf):
-        browser_type = __kupfer_settings__["browser_type"]
-        p = re.compile("^x")
-        r = p.match(leaf.object)
-        if r != None:
-            searchhwdb = [browser_type, 'https://nannyfe.prod.denic.de/search?Search=Search&searchpattern=%s' % leaf.object]
-        else:
-            searchhwdb = [browser_type, 'https://nannyfe.prod.denic.de/host/%s' % leaf.object]
-        utils.spawn_async(searchhwdb)
+    def wants_context(self):
+        return True
+
+    def activate_multiple(self, leav, iobjs):
+        for app in iobjs:
+            app.run(leav)
+
+    def activate(self, leaf, iobj, ctx):
+        self.activate_multiple(leaf.object, (iobj, ))
 
     def item_types(self):
         yield TextLeaf
@@ -47,5 +47,51 @@ class HwDB (Action):
         return _("Search hwdb in browser")
 
     def get_icon_name(self):
-        return "hwdb"
+        return "view-bank"
 
+    def requires_object(self):
+        return True
+
+    def object_types(self):
+        yield RunnableLeaf
+
+    def object_source(self, for_item=None):
+        return _GetActions()
+
+class _GetActions (Source):
+    def __init__(self):
+        super().__init__(name=_("Actions"))
+
+    def get_items(self):
+        yield Search()
+        yield Host()
+
+class Search (RunnableLeaf):
+    def __init__(self):
+        super().__init__(name=_("Search"))
+
+    def run(self, leaf):
+        browser_type = __kupfer_settings__["browser_type"]
+        searchhwdb = [browser_type, 'https://nannyfe.prod.denic.de/search?Search=Search&searchpattern=%s' % leaf]
+        utils.spawn_async(searchhwdb)
+
+    def item_types(self):
+        yield TextLeaf
+
+    def get_icon_name(self):
+        return "edit-find"
+
+class Host (RunnableLeaf):
+    def __init__(self):
+        super().__init__(name=_("Host"))
+
+    def run(self, leaf):
+        browser_type = __kupfer_settings__["browser_type"]
+        searchhwdb = [browser_type, 'https://nannyfe.prod.denic.de/host/%s' % leaf]
+        utils.spawn_async(searchhwdb)
+
+    def item_types(self):
+        yield TextLeaf
+
+    def get_icon_name(self):
+        return "node"
