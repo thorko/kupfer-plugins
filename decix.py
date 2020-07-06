cf17dbc210169804a070797903d7f2de11b37bd9
Thorsten Müller <thorko@thorko.de>
2020-07-06 12:54:59 +0200

    start and stop work easily

--- decix.py
+++ decix.py
@@ -1,15 +1,15 @@
-__kupfer_name__ = _("DE-CIX VPN")
-__kupfer_sources__ = ("VPN",)
-__description__ = _("handle VPN connections")
+__kupfer_name__ = _("DE-CIX")
+__kupfer_sources__ = ("WORK",)
+__description__ = _("handle DE-CIX work")
 __version__ = "2018-9"
 __author__ = "thorko"
 
 from kupfer.objects import Source, RunnableLeaf, TextLeaf
 from kupfer import utils, plugin_support, config
 
-class VPN (Source):
+class WORK (Source):
     def __init__(self):
-        super().__init__(_("DE-CIX VPN"))
+        super().__init__(_("DE-CIX WORK"))
 
     def initialize(self):
         pass
@@ -26,7 +26,7 @@ class VPN (Source):
 
     def description(self):
         return __description__
-    
+
     def get_icon_name(self):
         return "im-user"
 
@@ -35,8 +35,10 @@ class Start (RunnableLeaf):
         super().__init__(name=_("Start"))
 
     def run(self):
-        vpnstart=["/usr/local/bin/vpndecix.sh", "start"]
+        vpnstart=["/usr/bin/nmcli", "con", "up", "de-cix"]
+        timecalc=["/home/thorko/bin/work.sh", "start"]
         utils.spawn_async(vpnstart)
+        utils.spawn_async(timecalc)
 
     def get_icon_name(self):
         return "im-user-online"
@@ -47,8 +49,10 @@ class Stop (RunnableLeaf):
         super().__init__(name=_("Stop"))
 
     def run(self):
-        vpnstop=["/usr/local/bin/vpndecix.sh", "stop"]
+        vpnstop=["/usr/bin/nmcli", "con", "down", "de-cix"]
+        timecalc=["/home/thorko/bin/work.sh", "stop"]
         utils.spawn_async(vpnstop)
+        utils.spawn_async(timecalc)
 
     def get_icon_name(self):
         return "im-kick-user"
