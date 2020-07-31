49ffdef7b0576d0f149f08949d2c86fbd9e403e4
Thorsten Müller <thorko@thorko.de>
2020-07-31 09:52:42 +0200

    psono passwords

--- psono_passwords.py
+++ psono_passwords.py
@@ -1,48 +1,90 @@
-__kupfer_name__ = _("Psono Passwords")
-__kupfer_actions__ = ("PsonoPasswords", )
-__description__ = _("Search Psono Passwords")
+__kupfer_name__ = _("Psono")
+__kupfer_actions__ = ("PsonoPass", )
+__description__ = _("Copy password to clipboard (copyq)")
 __version__ = "2020-7"
 __author__ = "thorko"
 
-import os
-from kupfer.objects import Action, TextLeaf, Source
-from kupfer import utils, plugin_support, config
+import subprocess
+import json
+from kupfer.objects import Action, TextLeaf, Source, TextSource, Leaf
+from kupfer import utils, icons, plugin_support, kupferstring
 
-class PsonoPasswords (Action)
+class PsonoPass (Action):
     def __init__(self):
-        Action.__init__(self, _("Search for password")
+        Action.__init__(self, _("psono"))
 
     def is_factory(self):
         return True
 
     def activate(self, leaf):
-        return Search(leaf.object)
+        return SearchPass(leaf.object)
 
-    def item_type(self):
+    def item_types(self):
         yield TextLeaf
 
-    def description(self):
-        return __description__
+    def get_description(self):
+        return _("search for password")
 
     def get_icon_name(self):
         return "edit-find"
 
-class Search (Source):
+
+class SearchPass (TextSource):
+    def __init__(self, text):
+        TextSource.__init__(self, _("search password"))
+        self.text = text
+
+    def get_text_items(self, text):
+        command = ("psoco search -js '%s'" % self.text)
+        p1 = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        out, ignored_err = p1.communicate()
+        d = json.loads(out)
+        for l in d:
+            yield PassEntry(l['match']['name'], l['match']['name'], l['match']['id'])
+
+class PassEntry (Leaf):
+    def __init__(self, obj, name, pwid):
+        self.pwid = pwid
+        Leaf.__init__(self, obj, name)
+
+    def get_actions(self):
+        yield CopyPass()
+        yield CopyUser()
+
+    def get_icon_name(self):
+        return "dialog-password"
+
+class CopyUser(Action):
     def __init__(self):
-        Source.__init__(self, name=_('Results for "%s"') % query)
-        self.query = query
+        Action.__init__(self, _("Copy Username"))
 
-    def repr_key(self):
-        return self.query
+    def activate(self, leaf):
+        command = ("psoco user %s" % leaf.pwid)
+        p1 = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        out, ignored_err = p1.communicate()
+        utils.spawn_async(['copyq', 'add', out.decode("utf-8").strip('\n')])
 
-    def get_items(self):
-        c = ("pwc %s" % self.query)
-        p1 = subprocess.Popen(c, shell=True, stdout=subprocess.PIPE)
+    def get_icon_name(self):
+        return "edit-copy"
 
-        def get_locate_output(proc, offset=0):
-            out, ignored_err = proc.communicate()
-            return (ConstructFileLeaf(kupferstring.fromlocale(f))
-                    for f in out.split(b'\n')[offset:-1])
+    def get_description(self):
+        return _("Copy username to clipboard")
 
-        for F in get_locate_output(p1, 0):
-            yield F
+class CopyPass (Action):
+    def __init__(self):
+        Action.__init__(self, _("Copy Password"))
+
+    def activate(self, leaf):
+        command = ("psoco pwd %s" % leaf.pwid)
+        p1 = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        out, ignored_err = p1.communicate()
+        utils.spawn_async(['copyq', 'add', out.decode("utf-8").strip('\n')])
+
+    def get_description(self):
+        return _("Copy password to clipboard")
+
+    def get_icon_name(self):
+        return "edit-copy"
+
+    def item_types(self):
+        yield TextLeaf
